95793a69e61c32248dbc0acd6e3d802292f53824
Welisson Hudson Fernandes Lima <welisson.lima@capgemini.com>
2021-10-07 15:19:00 -0300

    Criação da classe CandidatoForm

--- src/main/java/com/projeto/urna/form/CandidatoForm.java
+++ src/main/java/com/projeto/urna/form/CandidatoForm.java
@@ -1,5 +1,42 @@
 package com.projeto.urna.form;
 
+import com.projeto.urna.model.Candidato;
+
 public class CandidatoForm {
 
+	private String nome;
+	private int numero;
+
+	public CandidatoForm() {
+	}
+
+	public CandidatoForm(String nome, int numero) {
+		this.nome = nome;
+		this.numero = numero;
+	}
+
+	public CandidatoForm(Candidato candidato) {
+		this.nome =  candidato.getNome();
+		this.numero = candidato.getNumero();
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	public Candidato converter() {
+		return new Candidato(nome, numero); 
+	}
 }
