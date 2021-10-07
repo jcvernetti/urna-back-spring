eaa87d3486185f1e1bca8db492001214165145a0
Welisson Hudson Fernandes Lima <welisson.lima@capgemini.com>
2021-10-07 14:37:57 -0300

    Criação da classe Candidato

--- src/main/java/com/projeto/urna/model/Candidato.java
+++ src/main/java/com/projeto/urna/model/Candidato.java
@@ -2,4 +2,40 @@ package com.projeto.urna.model;
 
 public class Candidato {
 
+	private int id;
+	private String nome;
+	private int numero;
+
+	public Candidato() {
+	}
+
+	public Candidato(int id, String nome, int numero) {
+		this.id = id;
+		this.nome = nome;
+		this.numero = numero;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
 }
