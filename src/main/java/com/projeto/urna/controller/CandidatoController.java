fdc190b6493f46122352b12f69a614c97eb7c859
Welisson Hudson Fernandes Lima <welisson.lima@capgemini.com>
2021-10-07 15:02:23 -0300

    Criação do endPoint getCandidatos da classe CandidatoController

--- src/main/java/com/projeto/urna/controller/CandidatoController.java
+++ src/main/java/com/projeto/urna/controller/CandidatoController.java
@@ -1,5 +1,25 @@
 package com.projeto.urna.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projeto.urna.model.Candidato;
+import com.projeto.urna.repository.CandidatoRepository;
+
+@RestController
+@RequestMapping("/candidato")
 public class CandidatoController {
 
+	@Autowired
+	CandidatoRepository candidatoRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Candidato>> getCandidatos(){
+		return ResponseEntity.ok().body(candidatoRepository.findAll());
+	}
 }
